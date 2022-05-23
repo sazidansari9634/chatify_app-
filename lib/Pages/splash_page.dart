ae42b1f7d1e2095ebc070f37450617042157b8a9
Sazid Ansari <sazidansari9634@gmail.com>
2022-05-23 16:46:48 +0545

    create cloud storage service

--- lib/Pages/splash_page.dart
+++ lib/Pages/splash_page.dart
@@ -8,6 +8,7 @@ import 'package:get_it/get_it.dart';
 //services
 import '../services/navigation_service.dart';
 import '../services/navigation_service.dart';
+import '../services/cloud_storage_service.dart';
 
 
 class SplashPage extends StatefulWidget{
@@ -68,6 +69,9 @@ class _SplashPageState extends State<SplashPage>{
      GetIt.instance.registerSingleton<NavigationService>(NavigationService(),);  
 
      GetIt.instance.registerSingleton<MediaService>(MediaService(),);
+
+    GetIt.instance.registerSingleton<CloudStorageService>(CloudStorageService(),);
+
 
   }
 
